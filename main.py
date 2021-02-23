e0ba0d6dbc7846a00c903c85572c5a37c679cb96
Alan Autrand <alan.autrand@gmail.com>
2021-02-22 22:26:23 -0700

    opening tif. doesnt work

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 from astropy.time import Time
 from astropy.coordinates import solar_system_ephemeris, EarthLocation
 from astropy.coordinates import get_body
-
+from PIL import Image
 
 from astroquery.jplhorizons import Horizons
 # This is a sample Python script.
@@ -29,11 +29,13 @@ def print_loc(name):
 
 
 def reading_tiffs():
+    img = Image.open("MSL_Gale_Orthophoto_Mosaic_25cm_v3.tif")
+    img.show()
     pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_loc('Astronomy!')
-
+    # >print_loc('Astronomy!')
+    reading_tiffs()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
